2da9b236bad1213038283e2069738ef33d6c8b7f
Rok Rozman <rok.rozman2@student.um.si>
2020-04-21 13:38:16 +0200

    Narejen Jarvisov obhod

--- Vaja2/Vaja2/Form1.cs
+++ Vaja2/Vaja2/Form1.cs
@@ -17,49 +17,45 @@ namespace Vaja2
             InitializeComponent();
         }
 
-        int stevilo_tock = 0;
-        bool dobili_st_tock;
+        int stevilo_tock = 0; // koliko tock moremo zgenerirati
         int nacin_generiranja = 1; // kateri nacin bo 2 je enakomerno, 1 je normalno
-        bool tocke_zgenerirane = false;
+        bool tocke_zgenerirane = false; // ce smo jih ze zgenerirali
 
-        public class Point
+        public class Tocka // moj class za tocko
         {
             public int x, y;
-            public Point(int x, int y)
+            public Tocka(int x, int y)
             {
                 this.x = x;
                 this.y = y;
             }
         }
 
-        List<Point> tocke = new List<Point>();
-        List<Point> lupina_tocke = new List<Point>();
+        List<Tocka> tocke = new List<Tocka>(); // kamor si shranim zgenerirane tocke
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             int i = int.Parse(textBox1.Text);
-            //MessageBox.Show("Test: " + i);
             stevilo_tock = i;
-            dobili_st_tock = true;
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             Brush aBrush = (Brush)Brushes.Black; // za risanje tock
             Pen pen = new Pen(ForeColor); // za risanje daljic med tockama
             Graphics g = this.CreateGraphics();
-
+        
             g.Clear(Color.White); // pocistim vse tocke in daljice ko je zacetek novih koordinat
 
             if (radioButton1.Checked == true)
             {
                 nacin_generiranja = 1;
-                MessageBox.Show("Normalno");
+                //MessageBox.Show("Normalno");
             }
             else if (radioButton2.Checked == true)
             {
                 nacin_generiranja = 2;
-                MessageBox.Show("Enakomerno");
+                //MessageBox.Show("Enakomerno");
             }
             else 
             {
@@ -69,31 +65,28 @@ namespace Vaja2
                 MessageBox.Show("Nisi vnesel stevila tock zato so avtomatsko 100");
                 stevilo_tock = 100;
             }
-            //MessageBox.Show("Stevilo tock : " + stevilo_tock);
+            
             if (nacin_generiranja == 1) 
             {
                 // NORMALNO
             }
             else if (nacin_generiranja == 2)
             {
+                
                 // ENAKOMETNO
                 Random rnd = new Random();
                 int x = 0, y = 0;
                 for (int i = 0; i < stevilo_tock; i++)
                 {
-                    x = rnd.Next(50, 650);
-                    y = rnd.Next(50, 500);
-                    tocke.Add(new Point(x, y));
+                    x = rnd.Next(50, 650); // random x koordinata
+                    y = rnd.Next(50, 500); // random y koordinata
+                    tocke.Add(new Tocka(x, y));
                     g.FillRectangle(aBrush, x, y, 2, 2); // narisemo tocko
                 }
-                MessageBox.Show("Narisal vse tocke!");
-               /* for (int i = 0; i < stevilo_tock; i++) {
-                    MessageBox.Show("Koordinate" + i + ".tocke: x: " + tocke[i].x + " y: " + tocke[i].y);
-                }*/
             }
 
             tocke_zgenerirane = true; // pomeni da smo zgenerirali tocke in lahko iscemo lupino
-
+        
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -105,39 +98,15 @@ namespace Vaja2
                 Pen pen = new Pen(ForeColor); // za risanje daljic med tockama
                 Graphics g = this.CreateGraphics();
 
-                //g.Clear(Color.White); // pocistim vse tocke in daljice ko je zacetek novih koordinat
-
                 if (radioButton3.Checked == true)
                 {
                     /// Jarvisov obhod
-                    MessageBox.Show("Jarvisov obhod");
-
-                    /// poisci najmanjso tocko
-                    int min_x = 99999, stevec_min_x=0;
-                    int index_tocke=0;
-                    MessageBox.Show("Stevilo tock: " + stevilo_tock);
-                    for (int i = 0; i < stevilo_tock; i++) { 
-                        if(tocke[i].x < min_x) 
-                        {
-                            min_x = tocke[i].x;
-                            // nasel tocko ki ima (trenutno) najnizjo x vrednost
-                            if(stevec_min_x == 0) 
-                            {
-                                // prva tocka zato si jo lahko kr shranim
-                                index_tocke = i;
-                                stevec_min_x++;
-                            }
-
-                            if (tocke[i].y < tocke[index_tocke].y) {
-                                // imamo tocko z novim najmanjsim y in je tot tocka ki je najmanjs trenutno
-                                index_tocke = i;
-                            }
-                        }
+                    Tocka[] a = new Tocka[stevilo_tock];
+                    for (int i = 0; i < stevilo_tock; i++) {
+                        a[i] = new Tocka(tocke[i].x, tocke[i].y); // si shranimo tocke v novo polje
                     }
 
-                    MessageBox.Show("Pobarval bom min tocko: ");
-                    g.FillRectangle(bBrush, tocke[index_tocke].x, tocke[index_tocke].y, 2, 2); // narisemo tocko
-                    lupina_tocke.Add(tocke[index_tocke]);
+                    Jarvisov_obhod(a, stevilo_tock); // klicemo funkcijo Jarisov obhod
 
                 }
                 else if (radioButton4.Checked == true)
@@ -151,5 +120,65 @@ namespace Vaja2
                 MessageBox.Show("Tocke se niso zgenerirane");
             }
         }
+
+
+        public static int orentacija(Tocka t1, Tocka t2, Tocka t3)
+        {
+            int val = (t2.y - t1.y) * (t3.x - t2.x) -
+                    (t2.x - t1.x) * (t3.y - t2.y);
+
+            if (val == 0) return 0; // kolinearna 
+            if (val > 0) { return 1; }
+            else { return 2; }
+        }
+
+
+        public void Jarvisov_obhod(Tocka[] tocke, int st)
+        {
+            // Shranimo lupino
+            List<Tocka> lupina = new List<Tocka>();
+
+            // Najdemo najbolj levo tocko
+            int leva = 0;
+            for (int i = 1; i < st; i++)
+            {
+                // gledamo da ima cim manjsi x
+                if (tocke[i].x < tocke[leva].x)
+                    leva = i;
+            }
+            int index = leva, nasledni;
+            do
+            {
+                lupina.Add(tocke[index]); // si dodamo tocko index v lupino
+
+                nasledni = (index + 1) % st; // izberemo naslednega in ce je index zadnji el gremo znova na 0
+
+                for (int i = 0; i < st; i++)
+                {
+                    // gremo skozi vse tocke
+                    if (orentacija(tocke[index], tocke[i], tocke[nasledni]) == 2)
+                    {
+                        nasledni = i; // si shranimo v nasledno
+                    }
+                }
+
+                index = nasledni; // si shranimo naslednega v index da si dodamo v lupino
+
+            } while (index != leva); // dokler ne pridemo do prve tocke
+            
+            // narisali bomo lupino
+            Brush aBrush = (Brush)Brushes.Black; // za risanje tock
+            Brush bBrush = (Brush)Brushes.Red; // za risanje tock
+            Pen pen = new Pen(ForeColor); // za risanje daljic med tockama
+            Graphics g = this.CreateGraphics();
+            for (int i = 0; i < lupina.Count(); i++)
+            {
+                // Narisemo daljice med tockami
+                if (i != lupina.Count() - 1) { g.DrawLine(pen, lupina[i].x, lupina[i].y, lupina[i + 1].x, lupina[i + 1].y); }
+                else { g.DrawLine(pen, lupina[i].x, lupina[i].y, lupina[0].x, lupina[0].y); }
+            }
+
+        }
+
     }
 }
